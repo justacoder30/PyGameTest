6cb7a04b025f5e1c54c81aa2aac2b6b1492bfe7e
justacoder30 <dangquang0129@gmail.com>
2024-11-17 14:44:56 +0700

    updated bg

--- Entity/BackGround.py
+++ Entity/BackGround.py
@@ -12,8 +12,7 @@ class BackGround:
         bg6 = pygame.transform.scale(pygame.image.load('resource/Background/layers/ground_2.png').convert_alpha(), Globals.Surface.get_size())
         bg7 = pygame.transform.scale(pygame.image.load('resource/Background/layers/plant.png').convert_alpha(), Globals.Surface.get_size())
 
-        # self.bg_list = [ bg1, bg2, bg3, bg4, bg5, bg6, bg7]
-        self.bg_list = [ bg1, bg2]
+        self.bg_list = [ bg1, bg2, bg3, bg4, bg5, bg6, bg7]
         self.bg = pygame.image.load('resource/Background/Background2.png').convert_alpha()
         self.bg = pygame.transform.scale(self.bg, Globals.Surface.get_size())
 
@@ -26,16 +25,9 @@ class BackGround:
         pass
 
     def Draw(self):
-        # self.speed = 0.6
-        # self.scroll = abs(Globals.camera.x * self.speed) % self.bg_width * -1
-        # for i in range(2):
-        #     Globals.Surface.blit(self.bg_list[5], (i * self.bg_width + int(self.scroll), 0))
                 
-        for i in range(2):
-            self.speed = 0.6
+        for i in range(7):
+            self.speed = 0.1
             for n in range(len(self.bg_list)):
-                self.scroll = abs(Globals.camera.x * self.speed) % self.bg_width * -1
-                Globals.Surface.blit(self.bg_list[n], (i * self.bg_width + self.scroll, 0))
-                if n==0:
-                    print(int(self.scroll), int(Globals.camera.x * self.speed))
+                Globals.Surface.blit(self.bg_list[n], (i * self.bg_width + Globals.camera.x * self.speed, 0))
                 self.speed+=0.1
